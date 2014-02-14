4ec10ea1bcf153b8c9b6f55c1fb27df72b4ee8b3
Matt Bergstedt <C16matthew.bergstedt@usafa.edu>
2014-02-14 12:53:18 -0700

    updated for subtraction

--- Lab2_Bergstedt.vhd
+++ Lab2_Bergstedt.vhd
@@ -32,6 +32,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Lab2_Bergstedt is
     Port ( Ain : in  STD_LOGIC_VECTOR (3 downto 0);
            Bin : in  STD_LOGIC_VECTOR (3 downto 0);
+			  Button : in STD_LOGIC;
            Sum : out  STD_LOGIC_VECTOR (3 downto 0));
 end Lab2_Bergstedt;
 
@@ -46,37 +47,50 @@ component Full_Adder is
 				Sum : out STD_LOGIC);
 end component Full_Adder;
 
+-- create a mux component for two's complement
+component Mux is
+		port (Button : in STD_LOGIC;
+				Bin : in STD_LOGIC_VECTOR(3 downto 0);
+				Bnew : out STD_LOGIC_VECTOR(3 downto 0));
+end component Mux;
+
 -- create internal wires for the carry between adders
 signal Co0, Co1, Co2, Co3 : std_logic;
+signal Bnew : std_logic_vector(3 downto 0);
 
 begin
+
+Mux0: component Mux
+	port map (Button => Button,
+				 Bin => Bin,
+				 Bnew => Bnew);
 
 -- instatiate 4 full adders
 Bit0: component Full_Adder
 	port map (Cin => '0',
 				 A => Ain(0),
-				 B => Bin(0),
+				 B => Bnew(0),
 				 Cout => Co0,
 				 Sum => Sum(0));
 	
 Bit1: component Full_Adder
 	port map (Cin => Co0,
 				 A => Ain(1),
-				 B => Bin(1),
+				 B => Bnew(1),
 				 Cout => Co1,
 				 Sum => Sum(1));
 
 Bit2: component Full_Adder
 	port map (Cin => Co1,
 				 A => Ain(2),
-				 B => Bin(2),
+				 B => Bnew(2),
 				 Cout => Co2,
 				 Sum => Sum(2));
 
 Bit3: component Full_Adder
 	port map (Cin => Co2,
 				 A => Ain(3),
-				 B => Bin(3),
+				 B => Bnew(3),
 				 Cout => Co3,
 				 Sum => Sum(3));
 
